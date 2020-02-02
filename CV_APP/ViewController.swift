4e895ce161c52b4d4a16ec0bf59cc8c9d1e3e45f
Bercik <bercikk13@gmail.com>
2020-02-02 14:09:45 +0100

    first init commit

---
+++ CV_APP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CV_APP
+//
+//  Created by Hubert Kuzioła on 02/02/2020.
+//  Copyright © 2020 HubertApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ CV_APPTests/CV_APPTests.swift
@@ -0,0 +1,34 @@
+//
+//  CV_APPTests.swift
+//  CV_APPTests
+//
+//  Created by Hubert Kuzioła on 02/02/2020.
+//  Copyright © 2020 HubertApps. All rights reserved.
+//
+
+import XCTest
+@testable import CV_APP
+
+class CV_APPTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
